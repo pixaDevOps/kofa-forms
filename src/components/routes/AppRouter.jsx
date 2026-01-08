589573251774b7dcf5dfda8fc69636050a594511
noolvinikhil24@gmail.com <noolvinikhil24@gmail.com>
2026-01-08 14:32:22 +0530

    membership integration with frontend and succsses message

--- src/components/routes/AppRouter.jsx
+++ src/components/routes/AppRouter.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import MembersReg from '../../components/pages/MembersReg'
-import DonationReg from '../../components/pages/DonationReg'
+import MembersReg from '../pages/MembersReg'
+import DonationReg from '../pages/DonationReg'
 import TopBar from '../layout/TopBar'
+import PaymentSuccess from '../pages/PaymentSuccess'
+
 const AppRouter = () => {
   return (
     <div>
@@ -11,6 +13,10 @@ const AppRouter = () => {
       <Routes>
         <Route path='/members-registration' element={<MembersReg/>}></Route>
         <Route path='/donation-registration' element={<DonationReg/>}></Route>
+        <Route
+          path="/payment-success/:applicationId"
+          element={<PaymentSuccess />}
+        />
       </Routes>
       </BrowserRouter>
     </div>
